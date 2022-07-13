a603821ffcbfc4d16f0c279d6206498000aab09d
Maxim Kovrov <maksim.kovrov@gmail.com>
2022-07-13 13:27:29 +0300

    🎥 add yt-dlp support for all supported URL for it

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 cd /app
 
+echo "deb http://deb.debian.org/debian/ bullseye-backports main contrib non-free" > /etc/apt/sources.list.d/backports.list
+apt update
+apt upgrade -y
+apt install -y yt-dlp
+
 _term() { 
   echo "Caught SIGTERM signal!" 
   kill -TERM "$child" 2>/dev/null
