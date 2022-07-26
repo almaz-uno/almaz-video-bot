8234b96af971ee81f0ea26e33ba2937b2edd6c06
Maxim Kovrov <maksim.kovrov@gmail.com>
2022-07-26 16:13:31 +0300

    🖊 remove unnecessary option

--- scripts/docker-run.bash
+++ scripts/docker-run.bash
@@ -7,10 +7,9 @@ APP_NAME=$(basename $(realpath .))
 . .env
 
 docker build -t $APP_NAME .
-
+    
 docker run -d \
     --name $APP_NAME \
-    -v /mnt/almaz-extractor-bot:/var/almaz-extractor-bot \
     -v /root/.acme.sh:/root/.acme.sh \
     --restart=unless-stopped \
     --privileged \
