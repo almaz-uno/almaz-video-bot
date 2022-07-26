22fef0a1cf43f5a115ee2803841a283a18bc21b7
Maxim Kovrov <maksim.kovrov@gmail.com>
2022-07-26 16:05:00 +0300

    📪 public access

--- scripts/docker-run-minio.bash
+++ scripts/docker-run-minio.bash
@@ -8,8 +8,8 @@ docker run -d \
     --name minio \
     --restart=unless-stopped \
     -v /var/minio:/data \
-    -p 10.8.5.1:9010:9000 \
+    -p 9010:9000 \
     -p 9011:9011 \
-    -e "MINIO_ROOT_USER=$MINIO_ACCESS" \
-    -e "MINIO_ROOT_PASSWORD=$MINIO_SECRET" \
+    -e "MINIO_ROOT_USER=$S3_ACCESS" \
+    -e "MINIO_ROOT_PASSWORD=$S3_SECRET" \
     quay.io/minio/minio server /data --console-address ":9011" \
