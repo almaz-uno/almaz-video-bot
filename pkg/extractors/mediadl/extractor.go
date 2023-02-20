bcb236d2bd81485551ebedf0f590a01dccd886ce
Maxim Kovrov <maksim.kovrov@gmail.com>
2023-02-20 23:51:29 +0300

    🧩 fix format for all purposes

--- pkg/extractors/mediadl/extractor.go
+++ pkg/extractors/mediadl/extractor.go
@@ -199,7 +199,8 @@ func (extractor *Extractor) publishFound(ctx context.Context, lg zerolog.Logger,
 
 	if chatID == 180727105 {
 		// myFormat := "best[ext=mp4][width<=640][filesize<200M]/worst[ext=mp4]"
-		myFormat := "mp4[width=640]/worst[ext=mp4]/ba"
+		// myFormat := "mp4[width=640]/worst[ext=mp4]/ba"
+		myFormat := "bestvideo*[width=640]+bestaudio/best"
 		go extractor.downloadMedia(ctx, lg, myFormat, &mm)
 	}
 }
