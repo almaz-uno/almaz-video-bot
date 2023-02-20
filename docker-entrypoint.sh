1de24413298f95c6bd63cb8290b6a7c4d7d44951
Maxim Kovrov <maksim.kovrov@gmail.com>
2023-02-20 23:09:10 +0300

    🥷 add plugins to yt-dlp

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,20 @@ cd /app
 echo "deb http://deb.debian.org/debian/ bullseye-backports main contrib non-free" > /etc/apt/sources.list.d/backports.list
 apt update
 apt upgrade -y
-apt install -y yt-dlp git
+apt install -y yt-dlp git python3-pip
 
-rm -fr /app/yt-dlp
-git clone https://github.com/yt-dlp/yt-dlp.git /app/yt-dlp
+pip install isodate
+
+test -d /app/yt-dlp || git clone https://github.com/yt-dlp/yt-dlp.git /app/yt-dlp
+git -C /app/yt-dlp pull
+
+test -d /app/yt-dlp-plugins || git clone https://github.com/almaz-uno/yt-dlp-plugins.git /app/yt-dlp-plugins
+git -C /app/yt-dlp-plugins pull
 
 ln -sf /app/yt-dlp/yt-dlp.sh /usr/bin/yt-dlp
+
+mkdir -p /etc/yt-dlp/plugins/almaz
+ln -sfT /app/yt-dlp-plugins/yt_dlp_plugins /etc/yt-dlp/plugins/almaz/yt_dlp_plugins
 
 _term() {
   echo "Caught SIGTERM signal!"
